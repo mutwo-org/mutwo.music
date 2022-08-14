c09b237e172dd1aaeb35e953615a16f48a2a9826
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2022-08-14 14:36:16 +0200

    Expose version to documentation

--- mutwo/music_version/__init__.py
+++ mutwo/music_version/__init__.py
@@ -1,4 +1,6 @@
-MAJOR, MINOR, PATCH = 0, 17, 0
-__version__ = f"{MAJOR}.{MINOR}.{PATCH}"
+MAJOR, MINOR, PATCH = 0, 17, 1
+
+VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
+"""The version of the package ``mutwo.music``."""
 
 del MAJOR, MINOR, PATCH
