6a5f2ca81e6e4e06b7ad4d6f46b0361ceaca4ea1
levinericzimmermann <levinericzimmermann@posteo.eu>
2022-11-13 12:00:53 +0100

    Update mutwo.core in nix to v.1.1.0

--- default.nix
+++ default.nix
@@ -3,10 +3,10 @@ with pkgs.python310Packages;
 
 let
 
-  mutwo-core-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.core/archive/61ebb657ef5806eb067f5df6885254fdbae8f44c.tar.gz";
+  mutwo-core-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.core/archive/83efe12fb98119e03db833c231f9c87956577b3f.tar.gz";
   mutwo-core = import (mutwo-core-archive + "/default.nix");
 
-  mutwo-common-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.common/archive/1dee56a8023de7ae37d1fca9475bd1e0a410b79d.tar.gz";
+  mutwo-common-archive = builtins.fetchTarball "https://github.com/mutwo-org/mutwo.common/archive/22c330fffb9b4a288d28422a512c7b8183d38f85.tar.gz";
   mutwo-common = import (mutwo-common-archive + "/default.nix");
 
   panphon = pkgs.python310Packages.buildPythonPackage rec {
