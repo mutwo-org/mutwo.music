37a6c3658231463078904ec50205b254a222c9f8
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-12-04 10:04:53 +0100

    Fix docstring

--- mutwo/music_events/configurations.py
+++ mutwo/music_events/configurations.py
@@ -12,13 +12,13 @@ from mutwo import music_parameters
 DEFAULT_PLAYING_INDICATORS_COLLECTION_CLASS = (
     music_parameters.PlayingIndicatorCollection
 )
-"""Default value for :attr:`~mutwo.ext.events.music.NoteLike.playing_indicator_collection`
+"""Default value for :attr:`mutwo.music_events.NoteLike.playing_indicator_collection`
 in :class:`~mutwo.music_events.NoteLike`"""
 
 DEFAULT_NOTATION_INDICATORS_COLLECTION_CLASS = (
     music_parameters.NotationIndicatorCollection
 )
-"""Default value for :attr:`~mutwo.ext.events.music.NoteLike.notation_indicator_collection`
+"""Default value for :attr:`mutwo.music_events.NoteLike.notation_indicator_collection`
 in :class:`~mutwo.music_events.NoteLike`"""
 
 
